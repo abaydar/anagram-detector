4c8e2f380a3c7483f8b92a021c9d98f4561310e1
+ <+>
2020-11-24 14:13:20 -0500

    commit tree 4465dac26b33c624f50f43dfc9467c62f014846e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+# Your code goes here
+require 'pry'
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words_array)
+        words_array.select do |w|
+            w.split("").sort == word.split("").sort             
+        end      
+
+
+    end
+
+end
+
+
+#input array of words
+#iterate over each element(word) in the array
+#split each word into individual letter array and compare
+#if true -> save into output array
